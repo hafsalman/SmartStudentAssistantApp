ef1114a8f5d777e17aee6b137fd0f20f3b2a575c
Hafsa Salman <hafsalman0521@gmail.com>
2026-03-27 17:54:48 +0500

    Fetching weather updates

---
+++ SSAA/src/services/weatherService.js
@@ -0,0 +1,24 @@
+export const fetchWeather = async (city) => {
+  try {
+    const API_KEY = "API_KEY";
+
+    const res = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},PK&appid=${API_KEY}&units=metric`
+    );
+
+    const data = await res.json();
+
+    if (data.cod !== 200) {
+      throw new Error("City not found");
+    }
+
+    return {
+      temp: data.main.temp,
+      condition: data.weather[0].main,
+      city: data.name,
+    };
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
